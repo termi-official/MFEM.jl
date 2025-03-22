06d8c339867aecb09179a25045805d43a71d2433
termi-official <termi-official@users.noreply.github.com>
2025-03-22 02:39:00 +0100

    Copy pasta oopsie

--- src/MFEM.jl
+++ src/MFEM.jl
@@ -14,36 +14,5 @@ module MFEM
 
     function __init__()
         @initcxx
-        #---Call Wrapper init--------------------------------------------------
-        G4JL_init()
-        #---Setup [data] environment-------------------------------------------
-        GEANT4_DATA_DIR = Base.get(ENV, "GEANT4_DATA_DIR", Geant4_jll.data_dir)
-        for line in readlines(joinpath(Geant4_jll.artifact_dir, "bin/geant4.sh"))
-            m = match(r"export[ ]+(G4.*)=.*/(.*)$", line)
-            if !isnothing(m)
-                G4JL_setenv(String(m[1]), joinpath(GEANT4_DATA_DIR, m[2]))
-            end
-        end
     end
-
-    include("SystemOfUnits.jl")
-    include("PhysicalConstants.jl")
-    
-    include("G4Utils.jl")
-    include("G4Scoring.jl")
-    include("G4CallBacks.jl")
-    include("G4ParticleGenerators.jl")
-    include("G4JLInterface.jl")
-
-    #---G4Vis--------------------------------------------------------------------------------------
-    export draw, draw!, drawDistanceToOut, G4JLEventDisplay
-    drawDistanceToOut() = "Not implemented"
-    draw() = "Not implemented"
-    draw!() = "Not implemented"
-    G4JLEventDisplay(::Int64) = "Not implemented"
-
-    #---G4Hist-------------------------------------------------------------------------------------
-    export H1D, H2D
-    H1D() = "Not implemented"  # Constructors
-    H2D() = "Not implemented"  # Constructors
 end
